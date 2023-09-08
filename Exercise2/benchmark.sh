983f7675804d6e7adbfb8064e6e907524f677f7e
Gabriele Codega@Orfeo <gabriele.codega@live.it>
2023-09-08 23:28:36 +0200

    time measurements for gemm

--- Exercise2/benchmark.sh
+++ Exercise2/benchmark.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
 #SBATCH --job-name=gemm_benchmark
 #SBATCH --partition=EPYC
+#SBATCH --exclusive
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --time=30
+#SBATCH --time=01:30:00
 
 module load architecture/AMD
 module load mkl/latest
 module load openBLAS/0.3.23-omp
 export LD_LIBRARY_PATH=/u/dssc/gcodeg00/myblis/lib:$LD_LIBRARY_PATH
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export BLIS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-touch data/$1_benchmark.csv
-echo "# m, n, k, time, gflops">>$1_benchmark.csv
+declare -a places=("threads" "cores" "numa_domains" "sockets" "ll_caches")
 
-for n in $(seq 2000 1000 20000)
-do
-    srun -n 1 ./gemm_$1.x $n $n $n >> $1_benchmark.csv
+export OMP_PROC_BIND=spread
+
+for place in "${places[@]}"
+do 
+    export OMP_PLACES=$place
+
+    fname=data/$1_benchmark_single_${OMP_PLACES}_${OMP_PROC_BIND}.csv
+
+    touch $fname
+    echo "# running on EPYC" >>$fname
+    echo "# thread allocation policy: $OMP_PLACES">>$fname
+    echo "# m, n, k, time, gflops">>$fname
+
+    for n in $(seq 2000 1000 20000)
+    do
+        for i in $(seq 1 5)
+        do
+            srun -n 1 ./gemm_$1.x $n $n $n >> $fname
+        done
+    done
 done
+
+squeue -j $SLURM_JOB_ID

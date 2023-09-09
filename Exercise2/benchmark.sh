90832e3f202130bffa858db621ccb7c89f6307e6
Gabriele Codega@Orfeo <gabriele.codega@live.it>
2023-09-09 22:12:18 +0200

    thin measurements and epyc double

--- Exercise2/benchmark.sh
+++ Exercise2/benchmark.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH --job-name=gemm_benchmark
-#SBATCH --partition=EPYC
+#SBATCH --partition=THIN
 #SBATCH --exclusive
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --time=01:30:00
+#SBATCH --cpus-per-task=12
+#SBATCH --time=02:00:00
 
-module load architecture/AMD
+module load architecture/Intel
 module load mkl/latest
 module load openBLAS/0.3.23-omp
 export LD_LIBRARY_PATH=/u/dssc/gcodeg00/myblis/lib:$LD_LIBRARY_PATH
@@ -23,10 +23,10 @@ for place in "${places[@]}"
 do 
     export OMP_PLACES=$place
 
-    fname=data/$1_benchmark_single_${OMP_PLACES}_${OMP_PROC_BIND}.csv
+    fname=data/thin/$1_benchmark_double_${OMP_PLACES}_${OMP_PROC_BIND}.csv
 
     touch $fname
-    echo "# running on EPYC" >>$fname
+    echo "# running on THIN" >>$fname
     echo "# thread allocation policy: $OMP_PLACES">>$fname
     echo "# m, n, k, time, gflops">>$fname
 
--- Exercise2/compile.sh
+++ Exercise2/compile.sh
@@ -3,4 +3,4 @@ module load architecture/AMD
 module load mkl/latest
 module load openBLAS/0.3.23-omp
 
-srun -n 1 -p EPYC make
+srun -n 1 make

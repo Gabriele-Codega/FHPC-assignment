6236b4b752cf367b4798d9edfe5fd46adc43c470
Gabriele Codega@Orfeo <gabriele.codega@live.it>
2023-09-19 11:24:24 +0200

    mkl thin numa

--- Exercise2/benchmark_single.sh
+++ Exercise2/benchmark_single.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH --job-name=gemm_benchmark
-#SBATCH --partition=EPYC
+#SBATCH --partition=THIN
 #SBATCH --exclusive
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=12
 #SBATCH --time=02:00:00
 
-module load architecture/AMD
+module load architecture/Intel
 module load mkl/latest
 module load openBLAS/0.3.23-omp
 export LD_LIBRARY_PATH=/u/dssc/gcodeg00/myblis/lib:$LD_LIBRARY_PATH
@@ -23,7 +23,7 @@ for place in "${places[@]}"
 do 
     export OMP_PLACES=$place
 
-    fname=data/epyc/$1_benchmark_single_${OMP_PLACES}_${OMP_PROC_BIND}.csv
+    fname=data/thin/$1_benchmark_single_${OMP_PLACES}_${OMP_PROC_BIND}_numa.csv
 
     touch $fname
     echo "# running on EPYC" >>$fname
